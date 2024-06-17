b0e9fb1dca6bd0a3d04b4ba2460502a4a0309260
Djallel BRAHMIA <d_brahmia@enst.dz>
2024-06-17 18:06:11 +0100

    Finish user cart and coupons

--- Controllers/cartController.js
+++ Controllers/cartController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Cart = require("../models/cartModel");
 const Product = require("../models/productModel");
+const Coupon = require("../models/couponModel");
 
 const ApiError = require("../utils/ApiErrors");
 
@@ -22,6 +23,7 @@ exports.getCart = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     status: "Success",
     data: cart,
+    numberOfItems: cart.cartitems.length,
   });
 });
 
@@ -63,7 +65,107 @@ exports.addProductToCart = asyncHandler(async (req, res, next) => {
     await cart.save();
     res.status(200).json({
       status: "Success",
+      numberOfItems: cart.cartitems.length,
+
       data: cart,
     });
   }
+});
+
+// @desc   Delete Product from user  cart
+// @route  Delete /api/v1/cart/:id
+// @access Private/Protect
+
+exports.deleteItemFromUserCart = asyncHandler(async (req, res, next) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return next(new ApiError("No cart for this user", 404));
+  }
+  cart.cartitems = cart.cartitems.filter(
+    (item) => item._id.toString() !== req.params.id
+  );
+  cart.totalCartPrice = calcTotalCartPrice(cart);
+  await cart.save();
+  res.status(200).json({
+    status: "Success",
+    numberOfItems: cart.cartitems.length,
+    data: cart,
+  });
+});
+
+// @desc   Delete Product from user  cart
+// @route  Delete /api/v1/cart/
+// @access Private/Protect
+exports.clearUserCart = asyncHandler(async (req, res, next) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return next(new ApiError("No cart for this user", 404));
+  }
+  cart.cartitems = [];
+  cart.totalCartPrice = 0;
+  await cart.save();
+  res.status(200).json({
+    status: "Success",
+    data: cart,
+  });
+});
+
+// @desc   Update item Quantity
+// @route  Put /api/v1/cart/
+// @access Private/Protect
+
+exports.updateItemQuantity = asyncHandler(async (req, res, next) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return next(new ApiError("No cart for this user", 404));
+  }
+  cart.cartitems = cart.cartitems.map((item) => {
+    if (item._id.toString() === req.params.id) {
+      item.quantity = req.body.quantity;
+    }
+    return item;
+  });
+  cart.totalCartPrice = calcTotalCartPrice(cart);
+  await cart.save();
+  res.status(200).json({
+    status: "Success",
+    numberOfItems: cart.cartitems.length,
+    data: cart,
+  });
+});
+
+// @desc   Apply Coupon on logged user  cart
+// @route  PUT /api/v1/cart/aaplycoupon
+// @access Private/Protect
+
+exports.applyCoupon = asyncHandler(async (req, res, next) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return next(new ApiError("No cart for this user", 404));
+  }
+  const coupon = await Coupon.findOne({
+    name: req.body.coupon,
+    expire: { $gt: Date.now() },
+  });
+  if (!coupon) {
+    return next(new ApiError("Coupon is invalid or expired", 404));
+  }
+
+  const totalCartPrice = calcTotalCartPrice(cart);
+  //Calculate total price after discount
+  const totalPriceAferDiscount = (
+    totalCartPrice -
+    (totalCartPrice * coupon.discount) / 100
+  ).toFixed(2);
+  cart.totalCartPrice = totalCartPrice;
+  cart.totalPriceAferDiscount = totalPriceAferDiscount;
+
+  await cart.save();
+  res.status(200).json({
+    status: "Success",
+    numberOfItems: cart.cartitems.length,
+    data: cart,
+    totalCartPrice,
+    totalPriceAferDiscount,
+  });
 });
--- Routes/CartRoute.js
+++ Routes/CartRoute.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { getCart, addProductToCart } = require("../Controllers/cartController");
+const {
+  getCart,
+  addProductToCart,
+  deleteItemFromUserCart,
+  clearUserCart,
+  updateItemQuantity,
+  applyCoupon,
+} = require("../Controllers/cartController");
 const { protect, allowedTo } = require("../Controllers/AuthController");
 
 const router = express.Router();
 
 router.use(protect, allowedTo("user"));
 
-router.route("/").get(getCart).post(addProductToCart);
-
+router
+  .route("/")
+  .get(getCart)
+  .post(addProductToCart)
+  .delete(clearUserCart)
+  .put(applyCoupon);
+router.route("/:id").delete(deleteItemFromUserCart).put(updateItemQuantity);
 module.exports = router;
